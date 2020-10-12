5191510d1d8bc529055c2b138a6b1484cd2a4e41
ThomasJones37 <71943233+ThomasJones37@users.noreply.github.com>
2020-10-12 12:10:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const copyright = document.getElementById("copyright");
 const date = document.getElementById("date");
 const expl = document.getElementById("expl");
 const zoom = document.getElementById("zoom");
+const link = document.getElementById("link");
+const subhead = document.getElementById("subhead");
+const vid = document.getElementById("vid");
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=';
+ 
+const url = 'https://api.nasa.gov/planetary/apod?&api_key=';
 const key = api.API_KEY
 
 const fetchNASAData = async () => {
@@ -22,14 +26,28 @@ const fetchNASAData = async () => {
 
 const displayData = data => {
     copyright.textContent = '© ' + ' ' + data.copyright;
-  
+    
     date.textContent = data.date;
-
-    img.src = data.hdurl;
 
     expl.textContent = data.explanation;
 
+    subhead.innerHTML = data.title;
+
+    img.src = data.hdurl;
+
     zoom.href = data.hdurl;
+
+    if (data.hdurl === undefined) {
+        console.log('Video not image today');
+        zoom.href = data.url;
+        link.href = data.url;
+        img.src = '';
+        link.classList.add('show');
+        
+    } 
+    if (data.copyright === undefined) {
+        copyright.textContent = 'No copyright';
+    }
     
 }
 
